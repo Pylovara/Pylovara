f1533448a15f86039030235373c517325988423a
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-26 12:45:12 +0200

    Create lsqt6ct.sh

---
+++ Polyvara-FileManager/lsqt6ct.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Pylovara Since 2025© https://github.com/Pylovara #
+#https://github.com/Pylovara/Hyprland-Module/Polyvara-FileManager
+
+source ~/.config/hypr/Polyvara-FileManager/header.sh
+cd ~/.config/qt6ct || exit 1
+tree_output=true
+show_header

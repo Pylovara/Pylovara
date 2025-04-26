cfb1416971c81b55f1d8522df5d397b82e8b3b6d
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-26 12:47:46 +0200

    Create lsyay.sh

---
+++ Polyvara-FileManager/lsyay.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Pylovara Since 2025© https://github.com/Pylovara #
+#https://github.com/Pylovara/Hyprland-Module/Polyvara-FileManager
+
+source ~/.config/hypr/Polyvara-FileManager/header.sh
+cd ~/.config/yay || exit 1
+tree_output=true
+show_header

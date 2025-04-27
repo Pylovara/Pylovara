93c4ca1ad164b5598a53bf47253a71c71ae05120
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-27 11:32:49 +0200

    Update aliasloader.sh
    Fixed better way :
    
    for f in ~/.config/hypr/Hyprland-Module/Polyvara-FileManager/ls*.sh; do
      name=$(basename "$f" .sh | sed 's/^ls//')
      alias "$name"="$f"
    done
    
    to :
    
    for f in "$HOME/.config/hypr/Hyprland-Module/Polyvara-FileManager"/ls*.sh; do
      [ -e "$f" ] || continue
      name="$(basename "${f}" .sh | sed 's/^ls//')"
      alias "${name}"="${f}"
    done

--- Polyvara-FileManager/aliasloader.sh
+++ Polyvara-FileManager/aliasloader.sh
@@ -2,9 +2,10 @@
 #Pylovara Since 2025© https://github.com/Pylovara #
 #https://github.com/Pylovara/Hyprland-Module/Polyvara-FileManager
 
-# Lädt automatisch alle ls*.sh-Skripte als Aliase, z.B. ~/.config/hypr/Polyvara-FileManager/lsupdate.sh → update
+# Lädt automatisch alle ls*.sh-Skripte als Aliase, z.B. ~/.config/hypr/Hyprland-Module/Polyvara-FileManager/lsupdate.sh → update
 
-for f in ~/.config/hypr/function/ls*.sh; do
-  name=$(basename "$f" .sh | sed 's/^ls//')
-  alias "$name"="$f"
+for f in "$HOME/.config/hypr/Hyprland-Module/Polyvara-FileManager"/ls*.sh; do
+  [ -e "$f" ] || continue
+  name="$(basename "${f}" .sh | sed 's/^ls//')"
+  alias "${name}"="${f}"
 done

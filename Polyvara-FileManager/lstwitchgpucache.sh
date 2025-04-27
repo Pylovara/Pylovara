4571a913913bf5aab2667d32a159fba20e6722ad
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-27 14:53:05 +0200

    Update lstwitchgpucache.sh

--- Polyvara-FileManager/lstwitchgpucache.sh
+++ Polyvara-FileManager/lstwitchgpucache.sh
@@ -2,7 +2,7 @@
 #Pylovara Since 2025© https://github.com/Pylovara #
 #https://github.com/Pylovara/Hyprland-Module/Polyvara-FileManager
 
-source ~/.config/hypr/Polyvara-FileManager/header.sh
+source ~/.config/hypr/Hyprland-Module/Polyvara-FileManager/header.sh
 cd ~/.config/Twitch/GPUCache || exit 1
 tree_output=true
 show_header

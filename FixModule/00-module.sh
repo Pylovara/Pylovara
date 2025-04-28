4b24bbae54a9337cb71e0ede461be9834ea2ac2e
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-28 22:53:29 +0200

    Update 00-module.sh
    fixed : 
    
    verlauf ... test

--- FixModule/00-module.sh
+++ FixModule/00-module.sh
@@ -3,23 +3,28 @@
 echo "https://github.com/Pylovara/Hyprland-Module/"
 echo "  === Start 00-module.sh Autorepair ===     "
 
-# Setzt Netzwerk, Sound, Xorg & Broadcom-Fix wenn nötig.
+# Scripte ausführbar machen .
+chmod +x 01-kernelandheader.sh
 chmod +x 02-NetzwerkUniversall.sh
 chmod +x 03-sound-fix.sh
 chmod +x 04-xorg-fix.sh
 
+# 1. Kernel und Header Aktualisieren
+echo "[1/5] Netzwerk-Fix wird gestartet..."
+./01-kernelandheader.sh
 
-# 1. Netzwerktreiber & Basis setzen
-echo "[1/6] Netzwerk-Fix wird gestartet..."
+# 2. Netzwerktreiber & Basis setzen
+echo "[2/5] Netzwerk-Fix wird gestartet..."
 ./02-NetzwerkUniversall.sh
 
-# 2. Sound-Probleme fixen (z. B. kein Output, PipeWire zickt)
-echo "[3/6] Sound-Fix wird gestartet..."
+# 3. Sound-Probleme fixen (z. B. kein Output, PipeWire zickt)
+echo "[3/5] Sound-Fix wird gestartet..."
 ./03-sound-fix.sh
 
-# 3. Xorg-Treiber (nur wenn man was mit X11 oder Nvidia etc. macht)
-echo "[4/6] Xorg-Fix wird gestartet..."
+# 4. Xorg-Treiber (nur wenn man was mit X11 oder Nvidia etc. macht)
+echo "[4/5] Xorg-Fix wird gestartet..."
 ./04-xorg-fix.sh
 
-# 6. Fertigmeldung
-echo "[6/6] AllInOne komplett durchgelaufen."
+# 5. Fertigmeldung
+echo "[5/5] AllInOne komplett durchgelaufen."
+

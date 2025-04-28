32c18e082aae72d56e38bd21a9298bbd127e4a1e
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-29 01:56:52 +0200

    Update aliasloader.sh

--- Pylovara-FileManager/KernModul/aliasloader.sh
+++ Pylovara-FileManager/KernModul/aliasloader.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Pylovara Since 2025© https://github.com/Pylovara #
-#https://github.com/Pylovara/Hyprland-Module/Pylovara-FileManager
+#Absoluter Ordnerpfad:                                               #
+#Hyprland-Module/Pylovara-FileManager/KernModul/aliasloader.sh       #
+#Pylovara Since 2025© https://github.com/Pylovara                    #
+#https://github.com/Pylovara/Hyprland-Module/Pylovara-FileManager    #  
 
 # Lädt automatisch alle ls*.sh-Skripte als Aliase, 
 # z.B. ~/.config/hypr/Hyprland-Module/Pylovara-FileManager/lsupdate.sh → update

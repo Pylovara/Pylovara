0349717bceadb641bc734821602a023e5ae12290
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-29 01:44:06 +0200

    Create aliasloader.sh
    KernModul läd alle alias inklusive update

---
+++ Pylovara-FileManager/KernModul/aliasloader.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Pylovara Since 2025© https://github.com/Pylovara #
+#https://github.com/Pylovara/Hyprland-Module/Pylovara-FileManager
+
+# Lädt automatisch alle ls*.sh-Skripte als Aliase, 
+# z.B. ~/.config/hypr/Hyprland-Module/Pylovara-FileManager/lsupdate.sh → update
+
+for f in "$HOME/.config/hypr/Hyprland-Module/Pylovara-FileManager"/ls*.sh; do
+  [ -e "$f" ] || continue
+  name="$(basename "${f}" .sh | sed 's/^ls//')"
+  alias "${name}"="${f}"
+done

41cec9ca30e275ecf3c3e94668bfb3dff848eaf2
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-29 12:32:13 +0200

    AHU
    man da wirste ja bekloppt ..... ich muss mir dafür ein script schreiben ey :-D

---
+++ Pylovara-FileManager/Search/P-pulse/lsppulse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Pylovara Since 2025© https://github.com/Pylovara #
+#https://github.com/Pylovara/Hyprland-Module/Pylovara-FileManager
+
+source ~/.config/hypr/Hyprland-Module/Pylovara-FileManager/KernModul/HeaderMaker.sh
+cd ~/.config/pulse || exit 1
+tree_output=true
+show_header

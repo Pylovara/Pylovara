91fbd6a637ae19a87eae045ee86eff89a4b7d793
Pylovara™ <55088442+Pylovara@users.noreply.github.com>
2025-04-30 18:21:47 +0200

    Create MainControl.sh
    für spätere zshrc anpassungen .... 4 sourced von hier rüber , zwei rein zwei raus .... 
    
    plus 1 alias ... der rest wird sich dann absofort hier abspielen

---
+++ Pylovara-FileManager/MainControl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Absoluter Pfad:
+# Hyprland-Module/Pylovara-FileManager/PylovaraMainControl.sh
+#
+# Pylovara Toolkit – aliasreset, aliasloader und aliaslist zentral ausführen
+
+# Tagging 
+modul_dir="$HOME/.config/hypr/Hyprland-Module/Pylovara-FileManager/"
+
+#   "============================"
+#   "   Pylovara MainControl     "
+#   "============================"
+
+#  1. 
+
+
+#  2. 
+
+
+#  3. 
+
+
+#  4.
+
+
+#  5.
+
+
+#  6.
+
+
+#  7.
+
+
+#  8.
+
+
+# 10.
+
+
+echo -e "\n[✓] Pylovara-MainControl"

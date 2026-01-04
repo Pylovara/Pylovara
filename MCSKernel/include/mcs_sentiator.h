8614f275bf017685a1640b1cad9a1d86d92f5afa
bandino <bandinozimmermann@gmail.com>
2026-01-04 12:25:23 +0100

    2026-01-04 12:25:23

--- MCSKernel/include/mcs_sentiator.h
+++
@@ -1,8 +0,0 @@
-#ifndef MCS_SENTIATOR_H
-#define MCS_SENTIATOR_H
-
-int mcs_sentiator_kann(int condition);     // ¶
-int mcs_sentiator_nicht(int condition);    // ¶¶
-int mcs_sentiator_negation(int value);     // °
-
-#endif
---
+++ System/Archive/MCSKernel0.2/include/mcs_wahrheiten.h
@@ -0,0 +1,10 @@
+#ifndef MCS_WAHRHEITEN_H
+#define MCS_WAHRHEITEN_H
+
+// Visualisierung
+void mcs_progress_operator(int stufe, int prozent);
+
+// Universelle Wahrheitsprüfung für den Sentiator
+int mcs_ist_wahr(const char *wert);
+
+#endif

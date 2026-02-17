5bc1c4a6b433636fdb2bf0480c54ced73d4697ff
bandino <bandinozimmermann@gmail.com>
2026-02-17 08:27:00 +0100

    2026-02-17 08:27:00

---
+++ Veraltet-testbereich/Core#Test3Closed/build/CMakeFiles/mcs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mcs.dir/link.d"
+  "CMakeFiles/mcs.dir/main.c.o"
+  "CMakeFiles/mcs.dir/main.c.o.d"
+  "mcs"
+  "mcs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mcs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Veraltet-testbereich/Core#Test3Closed/build/CMakeFiles/mcs.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for mcs.
+# This may be replaced when dependencies are built.
